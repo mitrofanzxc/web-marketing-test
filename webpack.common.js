8f05a04e90d678d3c62da5701fbf92af2ab77c5b
mitrofanzxc <mitrofanzxc@gmail.com>
2022-07-14 00:45:35 +0300

    feat: add header and nav sections (Thu, Jul 14, 2022 12:45:35 AM)

--- webpack.common.js
+++ webpack.common.js
@@ -22,6 +22,14 @@ module.exports = {
           from: path.resolve(__dirname, './src/images'),
           to: path.resolve(__dirname, './dist/images'),
         },
+        {
+          from: path.resolve(__dirname, './src/fonts'),
+          to: path.resolve(__dirname, './dist/fonts'),
+        },
+        {
+          from: path.resolve(__dirname, './src/shared'),
+          to: path.resolve(__dirname, './dist/shared'),
+        },
       ],
     }),
   ],
