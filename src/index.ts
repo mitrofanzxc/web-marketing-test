d60348bd9b2d773c586fda6aa6c59190a1f31afe
mitrofanzxc <mitrofanzxc@gmail.com>
2022-07-16 17:36:26 +0300

    feat: add logic for select-sort (Sat, Jul 16, 2022  5:36:26 PM)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
 import './index.scss';
+
+const SELECT_SORT = document.querySelector('.select-sort') as HTMLDivElement;
+const SELECT_SORT_BUTTON = document.querySelector('.select-sort__button') as HTMLButtonElement;
+const SELECT_SORT_BUTTON_DESCRIPTION = document.querySelector('.select-sort__button-description') as HTMLSpanElement;
+const SELECT_LIST = SELECT_SORT.querySelector('.select-list__wrapper') as HTMLDivElement;
+const SELECT_LIST_ITEMS = SELECT_LIST.querySelectorAll('.select-list__item') as NodeList;
+
+const addSelectActive = () => {
+  SELECT_LIST.classList.add('select-list__wrapper_active');
+};
+
+const removeSelectActive = () => {
+  SELECT_LIST.classList.remove('select-list__wrapper_active');
+};
+
+SELECT_SORT_BUTTON.addEventListener('click', addSelectActive);
+
+SELECT_LIST_ITEMS.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    const target = event.target as HTMLLIElement;
+    SELECT_SORT_BUTTON_DESCRIPTION.textContent = target.textContent;
+    removeSelectActive();
+  });
+});
